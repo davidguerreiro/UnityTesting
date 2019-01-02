2b1cff694e2c0a0d65ce47632f867ac1ebfccf20
davidguerreiro <me@davidguerreiro.com>
2019-01-02 17:11:59 +0100

    add cameraShake and change color Buttons scripts

---
+++ RotateWithMouse.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWithMouse : MonoBehaviour
+{
+    public float rotationSpeed;
+    void OnMouseDrag()
+    {
+        float x = Input.GetAxis( "Mouse X" ) * rotationSpeed;
+        float y = Input.GetAxis( "Mouse Y" ) * rotationSpeed;
+
+        // transform the Y axis.
+        transform.Rotate( Vector3.right, y );
+
+        // transform the X axis -  but default was inverted so we use a negative number.
+        transform.Rotate( Vector3.up, -x );
+    }
+}
